6bf95fb4c07bc48b97b054f045ae6195c33f5192
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-23 15:51:51 -0400

    main.py

--- main.py
+++ main.py
@@ -25,10 +25,16 @@ class MainPage(webapp2.RequestHandler):
         self.response.write(maintemp.render())
 class ExpensePage(webapp2.RequestHandler):
     def get(self):
+        #This is where we will ask the user to input monthly income and expenses
         expense_template = the_jinja_env.get_template("templates/expenses.html")
         self.response.write(expense_template.render())
+
 class BudgetPage(webapp2.RequestHandler):
-    def post(self):
+    def get(self):
+        #This is where we will ask the user to put either how much money they
+        #are willing to save per month or how many months they are willing to save.
+        #In addition, the budget page will retrieve the income and expense data
+        #from ExpensePage.
         Budget_list = Budget.query().fetch()
         total_expenses = self.request.get("INSERTEXPENSENAME")
         total_income = self.request.get("INSERTINCOMENAME")
@@ -37,11 +43,27 @@ class BudgetPage(webapp2.RequestHandler):
         budget_template = the_jinja_env.get_template("templates/budget.html")
         self.response.write(budget_template.render())
 
+    def post(self):
+        #This is where the page will post the remaining money (income-expenses)
+        expense_template = the_jinja_env.get_template("templates/budgetConfirm.html")
+        self.response.write(expense_template.render())
+    #def get(self):
+        #This is where we will ask the user to put either how much money they
+        #are willing to save per month or how many months they are willing to save.
+
+
+class budgetConfirm(webapp2.RequestHandler):
+    def post(self):
+        #This page will display the perviously entered amounts and then allow for
+        #a redirect to the BudgetPage with calculated results
+        expense_template = the_jinja_env.get_template("templates/budgetConfirm.html")
+        self.response.write(expense_template.render())
 
 
 app = webapp2.WSGIApplication([
     ("/", MainPage),
     ("/expenses", ExpensePage),
-    ("/budget", BudgetPage)
+    ("/budget", BudgetPage),
+    ("/budgetConfirm", budgetConfirmPage)
 
 ], debug=True)
