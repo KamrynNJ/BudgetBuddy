8ad84524cc1c122b594c61b356ba1cb9d7087f31
Kamryn Jones <kamjon2019@gmail.com>
2019-07-23 17:12:46 -0400

    adding more expenses

--- Index.js
+++ Index.js
@@ -1,11 +1,16 @@
 const addButton= document.querySelector('#add');
 
-function addItem(){
-    var ul = document.getElementById("expenses");
-    var text = document.getElementById("destext");
-    var li = document.createElement("li");
-    li.setAttribute('id',text.value);
-    li.appendChild(document.createTextNode(text.value));
-    ul.appendChild(li);
+var counter = 1;
+var limit = 10;
+function addInput(boxes){
+     if (counter == limit)  {
+          alert("You have reached the limit of adding " + counter + " inputs");
+     }
+     else {
+          var newdiv = document.createElement('div');
+          newdiv.innerHTML = "Expense " + (counter + 1) + "<br> <select><option value='Groceries'selected>Groceries</option><option value='Home'>Home and Utilites</option><option value='Debt'>Debt or Loans</option><option value='Tuition'>Tuition</option><option value='Health'> Health and Personal Care</option><option value='Entertainment'>Entertainment</option><option value='Other'>Other</option></select> <input id ='destext' type='text' name='describe' value=''> <input type='text' name='myInputs[]'>";
+          document.getElementById("boxes").appendChild(newdiv);
+          counter++;
+     }
 }
-addButton.addEventListener("click",addItem);
+addButton.addEventListener("click",addInput);
