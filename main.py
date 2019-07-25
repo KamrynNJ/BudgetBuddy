91fe042f8861c7e2a51a06c46acd00e193e5bbff
Kamryn Jones <kamjon2019@gmail.com>
2019-07-24 21:15:37 -0400

    complete list showing expense

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import jinja2
 import os
 from google.appengine.api import users
 
+
+
 # from classes import Budget
 the_jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -13,9 +15,12 @@ from google.appengine.ext import ndb
 
 class Budget(ndb.Model):
     expenses = ndb.StringProperty(required=True)
-    income = ndb.StringProperty(required=False)
     description=ndb.StringProperty(required=True)
     expense_amount=ndb.StringProperty(required=True)
+class Income(ndb.Model):
+    income = ndb.StringProperty(required=True)
+class Total(ndb.Model):
+    total_amount = ndb.StringProperty(required=True)
 class User(ndb.Model):
     email = ndb.StringProperty(required = True)
     user_id = ndb.StringProperty(required = True)
@@ -35,52 +40,71 @@ class ExpensePage(webapp2.RequestHandler):
 
 class BudgetPage(webapp2.RequestHandler):
     def get(self):
-        #This is where we will ask the user to put either how much money they
-        #are willing to save per month or how many months they are willing to save.
-        #In addition, the budget page will retrieve the income and expense data
-        #from ExpensePage.
-        # Budget_list = Budget.query().fetch()
-        # total_expenses = self.request.get("inserted_expense")
-        # total_income = self.request.get("amount")
-        # new_Budget = Budget(expenses = total_expenses, income = total_income)
-        # new_Budget.put()
-        budget_list = Budget.query().fetch()
         budget_template = the_jinja_env.get_template("templates/budget.html")
-        self.response.write(budget_template.render({"bud_list": budget_list,}))
 
-    def post(self):
-        blogs_template = the_jinja_env.get_template('templates/budget.html')
-        the_amount= self.request.get('amount')
-        the_des=self.request.get('description_of_thing')
-        the_expenses=self.request.get("dropdown")
-        the_income=self.request.get("income_added")
-
-        new_budget_entity = Budget(expenses = the_expenses,
-                                   description = the_des,
-                                   expense_amount = the_amount,
-                                   income=the_income
-                                   )
-        new_budget_entity.put()
-        budget_info=Budget.query().fetch()
-        self.response.write(blogs_template.render({'budget_info' : new_budget_entity}))
-
-
-        #This is where the page will post the remaining money (income-expenses)
-
+        budget_all=Budget.query().fetch()
+        income_all=Income.query().fetch()
+        total_all=Total.query().fetch()
+        self.response.write(budget_template.render({'budget_info': budget_all,
+                                                    'income_info':income_all[0],
+                                                    'total_info': total_all[0]}))
 
 
 class budgetConfirmPage(webapp2.RequestHandler):
     def post(self):
-        #This page will display the perviously entered amounts and then allow for
-        #a redirect to the BudgetPage with calculated results
-        expense_template = the_jinja_env.get_template("templates/budgetConfirm.html")
-        self.response.write(expense_template.render())
+        the_total=0;
+
+        blogs_template = the_jinja_env.get_template('templates/budget_confir.html')
+        the_amount= self.request.get('amount')
+        the_des=self.request.get('description_of_thing')
+        the_expenses=self.request.get("dropdown")
+        the_income=self.request.get("income_added")
+        the_counter=self.request.get("counter")
+
+
+
+        new_budget_entity = Budget(expenses = the_expenses,
+                                   description = the_des,
+                                   expense_amount = the_amount,
+                                   )
+        new_budget_entity.put()
+        the_total+=int(the_amount)
+
+        new_income_entity= Income(income=the_income)
+        new_income_entity.put()
+
+
+        if(int(the_counter)!=1):
+            for i in range(1,int(the_counter)+1):
+                the_amount2= self.request.get('myInputs['+str(i)+']')
+                the_des2=self.request.get('describe['+str(i)+']')
+                the_expenses2=self.request.get("drop["+str(i)+"]")
+                new_budget_entity2 = Budget(expenses = the_expenses2,
+                                              description = the_des2,
+                                              expense_amount = the_amount2,
+                                              )
+                new_budget_entity2.put()
+                the_total+=int(the_amount2)
+
+
+
+        budget_list = Budget.query().fetch()
+        the_total=int(the_income)-the_total
+        the_string_total=str(the_total)
+        new_total_entity= Total(total_amount=the_string_total)
+        new_total_entity.put()
+
+        self.response.write(blogs_template.render({'budget_info' : new_budget_entity,
+                                                   'budget_info2': budget_list,
+                                                   'income_info': new_income_entity,
+                                                   'total_info': new_total_entity}))
+
 
 
 app = webapp2.WSGIApplication([
     ("/", MainPage),
     ("/expenses", ExpensePage),
     ("/budget.html", BudgetPage),
-    ("/budget_confir.html", BudgetPage)
+    ("/budget_confir.html", budgetConfirmPage)
 
 ], debug=True)
