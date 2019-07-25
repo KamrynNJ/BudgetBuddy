b695d019a905a9743a172ab48e6861abe54e596a
Kamryn Jones <kamjon2019@gmail.com>
2019-07-24 21:16:05 -0400

    updated for expensise

--- style/Index.js
+++ style/Index.js
@@ -9,8 +9,9 @@ function addInput(boxes){
      else {
           var newdiv = document.createElement('div');
           newdiv.setAttribute("id", "boxes1");
-          newdiv.innerHTML = "<select name='drop'><option value='Groceries'selected>Groceries</option><option value='Home'>Home and Utilites</option><option value='Debt'>Debt or Loans</option><option value='Tuition'>Tuition</option><option value='Health'> Health and Personal Care</option><option value='Entertainment'>Entertainment</option><option value='Other'>Other</option></select> <input id ='destext' type='text' name='describe' placeholder='add a description' value=''> <input type='number' placeholder='0.00' step='0.01' name='myInputs[]'>";
+          newdiv.innerHTML = "<select name='drop["+counter+"]' ><option value='Groceries'selected>Groceries</option><option value='Home'>Home and Utilites</option><option value='Debt'>Debt or Loans</option><option value='Tuition'>Tuition</option><option value='Health'> Health and Personal Care</option><option value='Entertainment'>Entertainment</option><option value='Other'>Other</option></select> <input id ='destext' type='text' name='describe["+counter+"]' placeholder='add a description' value=''> <input type='number' placeholder='0.00' step='0.01' name='myInputs["+counter+"]'>";
           document.getElementById("boxes").appendChild(newdiv);
+          document.getElementById("counter_input").value=counter;
           counter++;
      }
 }
