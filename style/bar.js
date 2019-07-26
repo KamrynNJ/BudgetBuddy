1176affb4eb53031b6e116e772acb9f395c64937
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-26 14:57:40 -0400

    bar stuff

--- style/bar.js
+++ style/bar.js
@@ -1,3 +1,46 @@
+function showSavingsM1initial() {
+  var x = document.getElementById("oneMonthSavingDisplay");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showSavingsM2initial() {
+  var x = document.getElementById("twoMonthSavingDisplay");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showSavingsM6initial() {
+  var x = document.getElementById("sixMonthSavingDisplay");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showSavingsM12initial() {
+  var x = document.getElementById("twelveMonthSavingDisplay");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+showSavingsM1initial();
+showSavingsM2initial();
+showSavingsM6initial();
+showSavingsM12initial();
+
+
+
 function showSavingsM1() {
   var x = document.getElementById("oneMonthSavingDisplay");
   if (x.style.display === "none") {
